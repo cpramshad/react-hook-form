41d1e65da2b2a489f22adf94e7ace14da9320d68
Ramshad <cpramshad@gmail.com>
2024-11-17 12:22:15 +0400

    Register username with react hook form

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-// import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 export const YouTubeForm = () => {
-  // const form = useForm();
+  const form = useForm();
+  const { register } = form;
+
+  const { name, ref, onChange, onBlur } = register('username');
+
   return (
     <div>
       <h1>YouTube Form</h1>
 
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name={name}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
 
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
