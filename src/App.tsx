71d15a91ca90d8bf9ff3016dd309cd074b8e6f5e
Ramshad <cpramshad@gmail.com>
2024-11-17 12:00:24 +0400

    Add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 import { YouTubeForm } from './components/YouTubeForm';
 
 function App() {
   return (
     <div>
-      <YouTubeForm /> 
+      <YouTubeForm />
     </div>
   );
 }
--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
+// import { useForm } from 'react-hook-form';
 
 export const YouTubeForm = () => {
-    return (
-        <div>
-          <h1>YouTube Form</h1>
-    
-          <form>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
-    
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
-    
-            <label htmlFor="channel">Channel</label>
-            <input type="text" id="channel" name="channel" />
-    
-            <button>Submit</button>
-          </form>
-        </div>
-    );
-}
+  // const form = useForm();
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
